92f0080edb4671a3551aa9b018b3cb3a75407d50
Ayman Elsayed <31375408+AymanElsAhmed@users.noreply.github.com>
2022-07-04 11:47:19 +0200

    Add files via upload

---
+++ views/home.php
@@ -0,0 +1,35 @@
+
+<div class="container">
+    <div class="d-flex my-3">
+        <div class="me-auto p-3">Product List</div>
+        <a
+            type="button"
+            href="/addproduct"
+            class="btn btn-primary mx-1 text-center"
+        >
+            Add
+        </a>
+        <button type="button" class="btn btn-danger mx-1">Mass Delete</button>
+    </div>
+    <hr />
+    <div class="row mb-3 mt-3">
+        <div class="card text-center col-lg-3">
+            <div class="card-body">
+                <input
+                    class="form-check-input card-title d-start text-start"
+                    type="checkbox"
+                    value=""
+                    id="flexCheckDefault"
+                />
+                <h6 class="card-text">SKU</h6>
+                <h6 class="card-text">Name</h6>
+                <h6 class="card-text">Price</h6>
+                <h6 class="card-text">Size</h6>
+            </div>
+        </div>
+    </div>
+    <hr />
+</div>
+
+
+
